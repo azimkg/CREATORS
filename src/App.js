7e07b1d1c6f0c4f824b589e9ec078202106109ef
malikajusupova <malikajusupova0@gmail.com>
2022-05-30 16:22:06 +0600

    added works

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Works from "./components/Works/Works";
+import "antd/dist/antd.css";
 
 const App = () => {
-  return <div>dsdfsf</div>;
+  return (
+    <div>
+      <Works />
+    </div>
+  );
 };
 
 export default App;
