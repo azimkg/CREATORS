69b8b27ebec1cb4b63f0f9fc093c2885d0fab881
azimkg <azeldenbaev@gmail.com>
2022-05-31 16:23:34 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from "react";
 
-
 import Routing from "./Routing";
 import "./App.css";
 import "antd/dist/antd.css";
 
-
 const App = () => {
   return (
-    <div>
-
-
+    <div className="app">
       <Routing />
-
     </div>
   );
 };
