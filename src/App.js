c05b958e8dda92c516289b46af058f5bbf189bac
azimkg <100334196+azimkg@users.noreply.github.com>
2022-05-31 15:11:22 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
-import LandingCarousel from "./components/LandingCarousel/LandingCarousel";
-import LandingPage from "./components/LandingPage/LandingPage";
 
 const App = () => {
   return (
     <div>
-      <LandingPage />
-      <LandingCarousel />
+    
     </div>
   );
 };
