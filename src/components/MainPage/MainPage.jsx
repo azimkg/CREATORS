b180f84416ae5071187169855b8721afde25c8f5
azimkg <azeldenbaev@gmail.com>
2022-05-31 15:27:06 +0600

    Works add

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -6,39 +6,43 @@ import instagramImg from "../MainPage/MainPage-img/instagram.svg";
 import groupimg from "../MainPage/MainPage-img/Group1.png";
 import creators from "../MainPage/MainPage-img/CREATORS.png";
 import strelka from "../MainPage/MainPage-img/strelka.svg";
+import Works from "../Works/Works";
 
 const MainPage = () => {
   return (
-    <div className="main-page-content">
-      <div className="main-page-wrapper">
-        <div className="main-page-left-block">
-          <h1>
-            Awesome tools for Designer & Developer <span>.</span>{" "}
-          </h1>
-          <h5>
-            Antool is a web collection of information on paid or free Design and
-            Development tools
-          </h5>
-          <button className="main-page-button">
-            Order <img src={strelka} alt="" />{" "}
-          </button>
-          <div className="main-page-icon">
-            <img src={facebookImg} alt="" />
-            <img src={instagramImg} alt="" />
-            <img src={twitterImg} alt="" />
+    <>
+      <div className="main-page-content">
+        <div className="main-page-wrapper">
+          <div className="main-page-left-block">
+            <h1>
+              Awesome tools for Designer & Developer <span>.</span>{" "}
+            </h1>
+            <h5>
+              Antool is a web collection of information on paid or free Design
+              and Development tools
+            </h5>
+            <button className="main-page-button">
+              Order <img src={strelka} alt="" />{" "}
+            </button>
+            <div className="main-page-icon">
+              <img src={facebookImg} alt="" />
+              <img src={instagramImg} alt="" />
+              <img src={twitterImg} alt="" />
+            </div>
           </div>
-        </div>
-        <div className="main-page-rihgt-block">
-          <img className="main-page-group-img" src={groupimg} alt="" />
+          <div className="main-page-rihgt-block">
+            <img className="main-page-group-img" src={groupimg} alt="" />
 
-          <div className="main-page-creators-block">
-            <div className="main-page-creators">
-              <img src={creators} alt="" />
+            <div className="main-page-creators-block">
+              <div className="main-page-creators">
+                <img src={creators} alt="" />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <Works />
+    </>
   );
 };
 
