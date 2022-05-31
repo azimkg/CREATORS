b8f8cf12ae39141bae6d4ad22f582ba2956f74fd
omurkulovich <omurkulov.abs@gmail.com>
2022-05-31 12:31:02 +0600

    main-page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import MainPage from "./components/MainPage/MainPage";
 
 const App = () => {
-  return <div>dsdfsf</div>;
+  return (
+    <div>
+      <MainPage />
+    </div>
+  );
 };
 
 export default App;
